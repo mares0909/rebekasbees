8a65080ef595679231a0ea453d178d0ace29241f
mares0909 <mares0909@gmail.com>
2013-10-27 16:03:26 +0000

    Menu ready with dynamic and responsive galery

--- application.js
+++ application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("[role=content]").hide();
+	$("[role=content]>div").hide();
 	updateBgr();
 
 	var initial = true;
@@ -13,27 +13,56 @@ $(document).ready(function(){
 
 	function switchpage(e){
 		var selected = $(this).data("menu");
-		console.log(window.currentPage + "=?" + selected);
+		var cta = $(this).attr("class") == "cta menu" ? true : false ;
 		if(window.currentPage != selected) {
 			window.currentPage = selected;
 			$(".menu").removeClass("selected");
 			$("[data-menu=" + selected + "]").addClass("selected");
-			$("[role=content]").slideUp(650, function(){
-				$("[role=content]").load("pages/"+selected+".html", function(){
-					$(".cta").on('click', switchpage);
-				});
-			});
+			$("[role=content]>div").slideUp(650);
+			$("#" + selected).slideDown(650);
 			$("[role=hero_image]").animate({
 				height: "150",
 				backgroundPositionY: "-120"
-			}, 650, function(){
-				$("[role=content]").slideDown(650);
-			});
+			}, 650);
+
+			if( ($("nav[role=mobile]").is(":visible")) && (cta == false)) {
+				$("[role=mobile] #menu").slideToggle();
+			}
+		} else {
 			if($("nav[role=mobile]").is(":visible")) {
 				$("[role=mobile] #menu").slideToggle();
 			}
 		}
 	}
+
+	$("[data-galery]").on('click', function(e){
+		var slides = [];
+
+		$("#lightbox")
+			.css("display", "block")
+			.animate({
+				opacity: 1
+			},
+		650);
+		$(".swiper-wrapper").html("");
+		$.getJSON( "galery.json", function( data ) {
+			$.each( data.images, function( key, val ) {
+				$(".swiper-wrapper").append("<div class=\"swiper-slide\"><img src=\"galery/" + val.url + "\"><p class=\"title\">" + val.title + "</p><p>" + val.description + "</p></div>");
+			});
+			window.mySwiper = new Swiper('.swiper-container',{
+				loop:true,
+				grabCursor: true
+			})
+		});
+	});
+
+	$(".close").on('click', function(e){
+		$("#lightbox").animate({
+			opacity: 0
+		}, 650, function(){
+			$("#lightbox").css("display", "none");
+		});
+	});
 
 	$("#menu_cta").on('click', function(e){
 		$("[role=mobile] #menu").slideToggle();
@@ -43,4 +72,13 @@ $(document).ready(function(){
 		var width = $(window).width();
 		$("[role=hero_image]").css("background-position-x", -((1920-width)/2) );
 	}
+
+	$('.arrow-left').on('click', function(e){
+		e.preventDefault()
+		window.mySwiper.swipePrev()
+	})
+	$('.arrow-right').on('click', function(e){
+		e.preventDefault()
+		window.mySwiper.swipeNext()
+	})
 });
